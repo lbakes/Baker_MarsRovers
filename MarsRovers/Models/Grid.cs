a559fbe7468f7a25f6aa8c7532b38335ea894ff7
Lauren <bakerlauren671@gmail.com>
2021-02-14 10:31:22 -0700

    initial commit

---
+++ MarsRovers/Models/Grid.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MarsRovers.Models
+{
+    public class Grid
+    {
+        [Required]
+        [PosNumberNoZero(ErrorMessage = "Must be a positive number.")]
+        [DisplayName("Base")]
+        public int X { get; set; }
+
+        [Required]
+        [PosNumberNoZero(ErrorMessage = "Must be a postive number.")]
+        [DisplayName("Height")]
+        public int Y { get; set; }
+    }
+}
